8f062d04aa7f312ce315594c8c2694fe69cebec1
DanielPetukh <danielpetukh97@gmail.com>
2026-07-24 19:42:41 -0500

    Add files via upload

---
+++ password.py
@@ -0,0 +1,10 @@
+correctpassword="delsi"
+
+attempts=["hello","python","codingal","abcd"]
+
+for password in attempts:
+    if password==correctpassword:
+        print("This is the correct password. Wlecome in.")
+    else:
+        print("Wrong password")
+
